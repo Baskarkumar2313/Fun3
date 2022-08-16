eaded1fdb1d27fe2cde1579da4323190a0b8629a
BASKAR.K <110029191+Baskarkumar2313@users.noreply.github.com>
2022-08-16 15:53:44 +0530

    Add files via upload

---
+++ j.java
@@ -0,0 +1,25 @@
+/* 
+authour name:Baskar.K
+Program name:Static variable
+date:16-08-2022
+*/
+class j
+{
+static int value1;
+static float value2;
+static double value3;
+static boolean value4;
+static char value5;
+static long value6;
+public static void main(String[]args)
+{
+	int k=90;
+System.out.println("print the value:"+j.value1);
+System.out.println("print the value:"+j.value2);
+System.out.println("print the value:"+j.value3);
+System.out.println("print the value:"+j.value4);
+System.out.println("print the value:"+j.value5);
+System.out.println("print the value:"+j.value6);
+j obj=new j();
+}
+}
